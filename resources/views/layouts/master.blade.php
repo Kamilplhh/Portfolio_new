02c3b221d6b1c4ab5ac2dbef760cccf0406f5532
Kamilplhh <Kamil_PLHH@wp.pl>
2023-05-31 18:07:29 +0200

    Page progress

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Kamil_PLHH @yield('title')</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Annie+Use+Your+Telescope&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/js/app.js')
@@ -10,7 +11,10 @@
 
 <body>
     @section('navbar')
-
+        <marquee scrollamount="10" class="navbar top" direction="left">Linkedin github instagram</marquee>
+        <marquee scrollamount="10" class="navbar bottom" direction="right">Linkedin github instagram</marquee>
+        <marquee scrollamount="10" class="navbar left" direction="up">Linkedin github instagram</marquee>
+        <marquee scrollamount="10" class="navbar right" direction="down">Linkedin github instagram</marquee>
     @show
     <div class="content">
         @yield('content')
