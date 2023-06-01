4fd34b9e7493a9a6db4d8bea0577e7c9bdba6b1a
Kamilplhh <Kamil_PLHH@wp.pl>
2023-06-01 19:32:13 +0200

    Home page progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,21 @@
 <div class="projects center">
     PROJECTS
 </div>
+
 <div class="contact center">
     CONTACT
 </div>
+
 <div class="about center">
     ABOUT
 </div>
+
+<a href="https://github.com/Kamilplhh/Portfolio_new">
 <div class="mid center">
     SCRIPT
 </div>
+</a>
+
 <div class="services center">
     SERVICES
 </div>
