c89254ca0d495c2aee7c8a311b8799f20b1904a4
Kamilplhh <Kamil_PLHH@wp.pl>
2023-06-17 18:38:45 +0200

    Services controller

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -4,24 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\projects;
+use App\Models\services;
 use App\Interfaces\HistoryRepositoryInterface;
 
 class ProjectsController extends Controller
 {
     private HistoryRepositoryInterface $historyRepository;
 
-    public function __construct(HistoryRepositoryInterface $historyRepository) 
+    public function __construct(HistoryRepositoryInterface $historyRepository)
     {
         $this->historyRepository = $historyRepository;
     }
 
-    public function getData(){
+    public function getData()
+    {
 
         $jobs = $this->historyRepository->getJobsHistory();
         $educations = $this->historyRepository->getEducationHistory();
         $skills = $this->historyRepository->getSkills();
         $projects = Projects::all();
+        $servicesDone = services::where('status', '1')->get();
+        $servicesOutgoing = services::where('status', '0')->get();
 
-        return view('home', compact('projects', 'jobs', 'educations', 'skills'));
+        return view('home', compact('projects', 'jobs', 'educations', 'skills', 'servicesDone', 'servicesOutgoing'));
     }
 }
